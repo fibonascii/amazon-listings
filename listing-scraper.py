e4ca669db5b41b892159ccfe5632b2814890957c
Reagan Kirby <reaganjkirby@gmail.com>
2019-04-08 19:56:30 -0500

    Updating listing files

--- listing-scraper.py
+++ listing-scraper.py
@@ -4,11 +4,13 @@ import re
 import pandas
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from spaces_service import SpacesService
+import datetime
 
+service = SpacesService()
 
-listing_directory = os.path.basename(os.environ["LISTING_DIR"])
-listing_file = os.environ["LISTING_FILE"]
-listing_output_file = os.environ["LISTING_OUTPUT_FILE"]
+listing_directory = '/tmpproject/listing_files'
+listing_output_file = os.path.join(listing_directory, 'listing_output-{}.csv'.format(datetime.datetime.now()))
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -16,19 +18,27 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
+print(listing_output_file)
+
+
+def fetch_input_file():
+    input_file = service.download_listing_file()
+
+    return input_file
+
 
 def get_isbn(listing):
     """Grab the ISBN from the listing URL"""
-    match = re.search("/([0-9]{0,})/ref", str(listing))
+    match = re.search("/([0-9]*)/ref", str(listing))
     if match:
         isbn = match.group(1)
 
         return isbn
 
 
-def get_amazon_listings():
+def get_amazon_listings(input_file):
     """Iterate through listing excel sheet and build a list of all the Listing Links"""
-    df = pandas.read_excel(os.path.join(listing_directory, listing_file), header=0, names=["Listings"])
+    df = pandas.read_excel(os.path.join(listing_directory, input_file), header=0, names=["Listings"])
     amazon_listings = df["Listings"].to_list()
 
     return amazon_listings
@@ -36,7 +46,7 @@ def get_amazon_listings():
 
 def get_listing_prices(amazon_listings):
     """Iterate through the listing list and go to each listing to get the lowest fba price and write to csv"""
-    with open(os.path.join(listing_directory, listing_output_file), "w") as output_file:
+    with open(listing_output_file, "w") as output_file:
         header = ["ISBN", "Price"]
         writer = csv.DictWriter(output_file, fieldnames=header)
         writer.writeheader()
@@ -48,10 +58,17 @@ def get_listing_prices(amazon_listings):
                 lowest_fba_price = driver.find_element_by_class_name("olpOfferPrice").text
                 if isbn and lowest_fba_price:
                     writer.writerow({"ISBN": isbn, "Price": lowest_fba_price})
+                    print("ISBN: {} Price: {}".format(isbn, lowest_fba_price))
             except NoSuchElementException:
                 pass
 
 
+def upload_output_file(listing_output_file):
+    service.upload_file(listing_output_file)
+
+
 if __name__ == '__main__':
-    listings = get_amazon_listings()
+    listing_file = fetch_input_file()
+    listings = get_amazon_listings(listing_file)
     get_listing_prices(listings)
+    upload_output_file(listing_output_file)
---
+++ spaces_service.py
@@ -0,0 +1,46 @@
+import boto3
+import os
+from botocore.client import Config
+
+listing_file = os.environ['INPUT_FILE']
+
+
+class SpacesService:
+    def __init__(self):
+        self.session = boto3.session.Session()
+        self.client = self.session.client('s3',
+                                          region_name='sfo2',
+                                          endpoint_url='https://sfo2.digitaloceanspaces.com',
+                                          aws_access_key_id="VMLLWHDYIHOVKFMGKY2A",
+                                          aws_secret_access_key="74MBUH8ppW2lPyHNPXw5Cj8kZ3vQDblaSWlFBBUYmRY")
+
+    def list_all_buckets(self):
+        response = self.client.list_buckets()
+        spaces = [space['Name'] for space in response['Buckets']]
+        print("Spaces List: {}".format(spaces))
+
+        return spaces
+
+    def download_listing_file(self):
+        response = self.client.list_objects(Bucket='amazon-listings-storage')
+        for obj in response['Contents']:
+            if obj['Key'] == 'listing_files/input_files/{}'.format(listing_file):
+                self.client.download_file('amazon-listings-storage',
+                                          'listing_files/input_files/{}'.format(listing_file),
+                                          '/tmpproject/listing_files/{}'.format(listing_file))
+
+        if os.path.exists('/tmpproject/listing_files/{}'.format(listing_file)):
+            return listing_file
+
+    def upload_file(self, output_file):
+        response = self.client.upload_file(output_file,
+                                           'amazon-listings-storage',
+                                           'listing_files/output_files/{}'.format(os.path.basename(output_file)))
+
+        return response
+
+
+if __name__ == '__main__':
+    service = SpacesService()
+    service.list_all_buckets()
+    service.download_listing_file()
